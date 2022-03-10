cf8ed3db7cea6e5b153d81d0d573f1eba6d2850b
woonerb <woonerb@daum.net>
2022-03-10 23:18:36 +0900

    join.jsp login.jsp 완성

--- src/main/java/com/cos/blog/controller/UserController.java
+++ src/main/java/com/cos/blog/controller/UserController.java
@@ -1,5 +1,29 @@
 package com.cos.blog.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
 
+
+	//http://localhost:8000/blog/user/joinForm 가 오면
+	//  -->
+	// WEB-INF/views/user/joinForm.jsp  로보내준다
+	@GetMapping("/user/joinForm")
+	public String joinForm() {
+		
+		return "user/joinForm";
+	}
+	
+	
+	//http://localhost:8000/blog/user/loginForm 가 오면
+	//  -->
+	// WEB-INF/views/user/loginForm.jsp  로보내준다
+	@GetMapping("/user/loginForm")
+	public String loginForm() {
+		
+		return "user/loginForm";
+	}
+	
 }
---
+++ src/main/webapp/WEB-INF/views/layout/footer.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<br />
+<div class="jumbotron text-center" style="margin-bottom: 0">
+	<p>Created by 2200247</p>
+	<p>053-740-2570</p>
+	<p>Daegu</p>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
